6e19cf93da6f4b6d663f81f20906ebcaad3911ac
emeliesv <emeliesv@live.se>
2024-05-29 13:32:29 +0200

    shoppingcart in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ProductList from "./Components/Productlist";
+import ShoppingCart from "./Components/Shoppingcart";
 
 import {
   useQuery,
@@ -15,6 +16,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <main>
         <h1>HEJ JENYF</h1>
+        <ShoppingCart />
         <ProductList />
       </main>
     </QueryClientProvider>
