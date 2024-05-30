859ad5daf23cbbcf8cd80f432cc2aa761bc10114
johannabra <johanna.branting@chasacademy.se>
2024-05-30 11:08:34 +0200

    header and nav first draft

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,1 +1,16 @@
 /* HEADER containing navbar and logo */
+import Nav from "./Nav";
+import CartIcon from "./CartIcon";
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center mx-20">
+      <h1 className="font-bold">Jenyf Studios</h1>
+      <div className="flex flex-row items-center">
+        <Nav />
+        <CartIcon />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
