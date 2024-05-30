f1ff3d90204efba6215f93450913d54a91209f68
Fredtorn <fredrika.tornkvist@chasacademy.se>
2024-05-30 18:15:14 +0200

    added base layout structure to landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,5 +1,25 @@
 const Landing = () => {
-  return <>Landing page</>
+  return (
+    <main className="mx-40 my-20">
+      <div className="bg-gray-400 w-full h-80">Hero image</div>
+      <h1 className="text-center text-5xl font-black p-10">Title</h1>
+      <p className="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla est ullamcorper eget nulla facilisi etiam. Ac turpis egestas integer eget aliquet nibh praesent. Sit amet aliquam id diam maecenas ultricies mi eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla est ullamcorper eget nulla facilisi etiam. Ac turpis egestas integer eget aliquet nibh praesent. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
+      <div className="flex flex-row w-full justify-between my-20">
+        <span className="w-80 ">
+          <div className="bg-gray-400 w-full h-96"></div>
+          <h3 className="text-center p-4 text-base">Category</h3>
+        </span>
+        <span className="w-80 ">
+          <div className="bg-gray-400 w-full h-96"></div>
+          <h3 className="text-center p-4 text-base">Category</h3>
+        </span>
+        <span className="w-80 ">
+          <div className="bg-gray-400 w-full h-96"></div>
+          <h3 className="text-center p-4 text-base">Category</h3>
+        </span>
+      </div>
+    </main>
+  )
 };
 
 export default Landing;
