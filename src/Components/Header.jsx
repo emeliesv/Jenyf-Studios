7674cb0d92c43cd8ec55ee8596e36eb949a10abf
emeliesv <emeliesv@live.se>
2024-05-31 08:44:33 +0200

    header responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,9 +5,17 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center mx-10">
       <h1 className="font-bold">Jenyf Studios</h1>
-      <CartIcon />
-      <div className="flex flex-row items-center">
-        <Nav />
+      <div className="hidden md:flex">
+        <div className="flex flex-row items-center">
+          <Nav />
+        </div>
+        <CartIcon />
+      </div>
+      <div className="flex sm:hidden">
+        <CartIcon />
+        <div className="flex flex-row items-center">
+          <Nav />
+        </div>
       </div>
     </header>
   );
