175460ec62260556e42d599886d7d5af81dc057e
Nabila Maksud <nabila.maksud@gmail.com>
2024-06-02 18:41:41 +0200

    Product card's first Impresion

--- src/Components/Productlist.jsx
+++ src/Components/Productlist.jsx
@@ -1,34 +1,31 @@
 import { useProducts } from "../Context/ProductContext";
-import { CartContext } from "../Context/CartContext";
-import { useContext } from "react";
+import ProductCard from "./ProductCard";
+import { useState } from "react";
 
 const ProductList = () => {
   const { data, isLoading, isError } = useProducts();
-  const { addToCart } = useContext(CartContext);
+  const [category, setCategory] = useState(``)
+
 
   if (isError) return <h1>Failed to fetch product</h1>;
   if (isLoading) return <h1>Loading...</h1>;
 
-  return (
-    <div>
-      {data &&
-        data.map((product) => {
-          return (
-            <div key={product.id} className="mb-4 mt-4">
-              <h1 className="font-bold">{product.title}</h1>
-              <img src={product.image} alt={product.title} className="w-40" />
-              <p>{product.description}</p>
-              <div>{product.category}</div>
-              <div> ${product.price}</div>
-              <div>
-                {product.rating.rate} ({product.rating.count} reviews)
-              </div>
-              <button onClick={() => addToCart(product)}>Add to cart</button>
-            </div>
-          );
-        })}
-    </div>
+  return (<>
+    <fieldset className=" border">
+      <legend>Select Category</legend>
+      <select value={category} onChange={(e) => { setCategory(e.target.value) }}>
+        <option value={''}>Select One</option>
+        <option value={`men's clothing`}>Men</option>
+        <option value={`women's clothing`}>Women</option>
+        <option value={`jewelery`}>Jewelery</option>
+        <option value={`electronics`}>Electronics</option>
+      </select>
+    </fieldset>
+    <div className="grid gap-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1">
+      {data && data.map((product) => { return ((category === product.category) || category === '' ? (<ProductCard key={product.id} product={product} />) : (null)) })}
+    </div >
+  </>
   );
 };
 
-export default ProductList;
+export default ProductList;
