98a2d2623cf0e34b6630317afb09661541a6272e
Fredtorn <fredrika.tornkvist@chasacademy.se>
2024-06-11 11:24:55 +0200

    added styling to header and resized logo

--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CategoryCard = ({ category, image }) => {
   return (
-    <span className="bg-white w-80">
+    <span className="w-80">
       <img src={image} className="h-96 w-full" />
       <h3 className=" text-center p-4 text-base">{category}</h3>
     </span>
