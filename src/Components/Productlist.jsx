070dbae228c2165e04a60135f883bb05c9de0887
Nabila Maksud <nabila.maksud@gmail.com>
2024-06-13 06:36:18 +0200

    Hello I have added md:grid-cols-2 xs:grid-cols-1 for cad resposivness at Product card list and updated the product card

--- src/Components/Productlist.jsx
+++ src/Components/Productlist.jsx
@@ -1,31 +1,62 @@
 import { useProducts } from "../Context/ProductContext";
 import ProductCard from "./ProductCard";
+import { useNavigate } from "react-router-dom";
+import { Pagination } from "@mui/material";
 import { useState } from "react";
 
-const ProductList = () => {
+const ProductList = ({ selectedCategory }) => {
   const { data, isLoading, isError } = useProducts();
-  const [category, setCategory] = useState(``)
+  const navigate = useNavigate();
+  const [page, setPage] = useState(1);
 
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
   if (isError) return <h1>Failed to fetch product</h1>;
   if (isLoading) return <h1>Loading...</h1>;
 
-  return (<>
-    <fieldset className=" border">
-      <legend>Select Category</legend>
-      <select value={category} onChange={(e) => { setCategory(e.target.value) }}>
-        <option value={''}>Select One</option>
-        <option value={`men's clothing`}>Men</option>
-        <option value={`women's clothing`}>Women</option>
-        <option value={`jewelery`}>Jewelery</option>
-        <option value={`electronics`}>Electronics</option>
-      </select>
-    </fieldset>
-    <div className="grid gap-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1">
-      {data && data.map((product) => { return ((category === product.category) || category === '' ? (<ProductCard key={product.id} product={product} />) : (null)) })}
-    </div >
-  </>
+  const filteredData = selectedCategory
+    ? data.filter((product) => product.category === selectedCategory)
+    : data;
+
+  const handleCategoryChange = (e) => {
+    const selected = e.target.value;
+    if (e.target.value === "") {
+      navigate("/products");
+    } else {
+      navigate(`/products/category/${selected}`);
+    }
+  };
+
+  return (
+    <>
+      <fieldset className=" border">
+        <legend>Select Category</legend>
+        <select value={selectedCategory || ""} onChange={handleCategoryChange}>
+          <option value={""}>Select One</option>
+          <option value={`men's clothing`}>Men</option>
+          <option value={`women's clothing`}>Women</option>
+          <option value={`jewelery`}>Jewelery</option>
+          <option value={`electronics`}>Electronics</option>
+        </select>
+      </fieldset>
+      <div className="grid gap-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 xs:grid-cols-1">
+        {filteredData.length > 0 ? (
+          filteredData
+            .slice(page * 4 - 4, page * 4)
+            .map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+        ) : (
+          <p>No products found for this category</p>
+        )}
+      </div>
+      <div>
+        <Pagination count={5} page={page} onChange={handlePageChange} />
+      </div>
+    </>
   );
 };
 
-export default ProductList;
+export default ProductList;
